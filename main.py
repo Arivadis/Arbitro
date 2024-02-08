852dafe5c13298e765369df90e7c69709a04eee2
Vladyslav Archivadze <arivadis@ukr.net>
2024-02-08 11:50:02 +0100

    Updated asynch db

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import ccxt.pro
-from asyncio import gather, run
-import sqlite3
+from asyncio import gather, run, sleep
+import aiosqlite
 import datetime
 
 orderbooks = {}
 
 
-def handle_all_orderbooks(orderbooks):
+async def handle_all_orderbooks(orderbooks):
     print('We have the following orderbooks:')
-    for id, orderbooks_by_symbol in orderbooks.items():
-        for symbol in orderbooks_by_symbol.keys():
-            orderbook = orderbooks_by_symbol[symbol]
-            timestamp = datetime.datetime.now()
-            asks_price, asks_volume = orderbook['asks'][0]
-            bids_price, bids_volume = orderbook['bids'][0]
-            print(timestamp, id, symbol, asks_price, asks_volume, bids_price, bids_volume)
-            # Insert into SQLite
-            insert_into_db(timestamp, id, symbol, asks_price, asks_volume, bids_price, bids_volume)
+    async with aiosqlite.connect('orderbook.db') as conn:
+        async with conn.cursor() as c:
+            await c.execute('''CREATE TABLE IF NOT EXISTS orderbooks 
+                                (timestamp TEXT, id TEXT, symbol TEXT, asks_price REAL, 
+                                asks_volume REAL, bids_price REAL, bids_volume REAL)''')
+            await conn.commit()
+            for id, orderbooks_by_symbol in orderbooks.items():
+                for symbol in orderbooks_by_symbol.keys():
+                    orderbook = orderbooks_by_symbol[symbol]
+                    timestamp = datetime.datetime.now()
+                    asks_price, asks_volume = orderbook['asks'][0]
+                    bids_price, bids_volume = orderbook['bids'][0]
+                    print(timestamp, id, symbol, asks_price, asks_volume, bids_price, bids_volume)
+                    # Insert into SQLite
+                    await c.execute("INSERT INTO orderbooks VALUES (?, ?, ?, ?, ?, ?, ?)",
+                                    (timestamp, id, symbol, asks_price, asks_volume, bids_price, bids_volume))
+            await conn.commit()
 
-def insert_into_db(timestamp, id, symbol, asks_price, asks_volume, bids_price, bids_volume):
-    conn = sqlite3.connect('orderbook.db')
-    c = conn.cursor()
-    # Create table if not exists
-    c.execute('''CREATE TABLE IF NOT EXISTS orderbooks 
-                 (timestamp TEXT, id TEXT, symbol TEXT, asks_price REAL, asks_volume REAL, bids_price REAL, bids_volume REAL)''')
-    # Insert data
-    c.execute("INSERT INTO orderbooks VALUES (?, ?, ?, ?, ?, ?, ?)",
-              (timestamp, id, symbol, asks_price, asks_volume, bids_price, bids_volume))
-    conn.commit()
-    conn.close()
+
 async def symbol_loop(exchange, id, symbol):
     print('Starting', id, symbol)
     while True:
@@ -39,16 +37,7 @@ async def symbol_loop(exchange, id, symbol):
             orderbooks[id] = orderbooks.get(id, {})
             orderbooks[id][symbol] = orderbook
             print('===========================================================')
-            #
-            # here you can do what you want
-            # with the most recent versions of each orderbook you have so far
-            #
-            # you can also wait until all of them are available
-            # by just looking into all the orderbooks and counting them
-            #
-            # we just print them here to keep this example simple
-            #
-            handle_all_orderbooks(orderbooks)
+            await handle_all_orderbooks(orderbooks)
         except Exception as e:
             print(str(e))
             break  # you can break just this one loop if it fails
@@ -65,12 +54,9 @@ async def exchange_loop(id, config):
 
 
 async def main():
-    conn = sqlite3.connect('orderbook.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS orderbooks 
-                 (timestamp TEXT, id TEXT, symbol TEXT, asks_price REAL, asks_volume REAL, bids_price REAL, bids_volume REAL)''')
-    conn.commit()
-    conn.close()
+    conn = await aiosqlite.connect('orderbook.db')
+    print("Connected to database.")
+
     configs = {
         'Binance spot': {
             'id': 'binance',
@@ -87,8 +73,23 @@ async def main():
             },
         },
     }
-    loops = [exchange_loop(id, config) for id, config in configs.items()]
-    await gather(*loops)
+    try:
+        loops = [exchange_loop(id, config) for id, config in configs.items()]
+        await gather(*loops)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        await conn.close()
+        print("Database connection closed.")
 
+async def shutdown():
+    print("Shutting down...")
+    # Add any cleanup operations here
 
-run(main())
+# Run the main function and handle graceful shutdown
+try:
+    run(main())
+except KeyboardInterrupt:
+    pass
+finally:
+    run(shutdown())
